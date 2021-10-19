7e9656171fd13655fffd5b9d17ea13833c48bdc3
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-10-20 00:25:10 +0300

    LAB-3 Add task 3 and refactor

--- lab_3/src/task1.cpp
+++ lab_3/src/task1.cpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <iostream>
 
-void wzAsm(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
+void wzAsm(uint32_t x, uint32_t y, uint32_t z, uint32_t w)
 {
   asm(
       "addl $79, %%eax\n"
@@ -15,9 +15,9 @@ void wzAsm(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
   std::cout << "z: " << z << " w: " << w << "\n\n";
 }
 
-void wzAsmWithPointers(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
+void wzAsmWithPointers(uint32_t x, uint32_t y, uint32_t z, uint32_t w)
 {
-  uint64_t *p = &x, *q = &y;
+  uint32_t *p = &x, *q = &y;
   asm(
       "movl (%[P]), %%edx\n" //edx = *eax и по какой-то причине просто с (%%eax) оно не работало
       "movl %%edx, %%eax\n"  //eax = edx
@@ -34,13 +34,21 @@ void wzAsmWithPointers(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
   std::cout << "z: " << z << " w: " << w << "\n\n";
 }
 
-void wzAsmWithoutReferences(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
+void wzAsmWithoutReferences(uint32_t x, uint32_t y, uint32_t z, uint32_t w)
 {
+  asm(
+      "addl $79, %k2\n"
+      "xor %k1, %k1\n"
+      "divl %k4\n"
+      : "=a"(z), "=d"(w)
+      : "0"(x), "1"(42), "r"(y)
+      : "cc");
+
   std::cout << "ASM without references:" << std::endl;
   std::cout << "z: " << z << " w: " << w << "\n\n";
 }
 
-void wz(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
+void wz(uint32_t x, uint32_t y, uint32_t z, uint32_t w)
 {
   z = (x + 79) / y;
   w = (x + 79) % y;
@@ -51,10 +59,11 @@ void wz(uint64_t x, uint64_t y, uint64_t z, uint64_t w)
 
 int main(int argc, char **argv)
 {
-  uint64_t x = 10, y = 5, z, w;
+  uint32_t x = 10, y = 5, z, w;
 
   wzAsm(x, y, z, w);
   wzAsmWithPointers(x, y, z, w);
+  wzAsmWithoutReferences(x, y, z, w);
   wz(x, y, z, w);
   return 0;
 }
