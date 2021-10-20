3975ca519827ff0c9ef13591571ccfda97770f07
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-10-20 17:14:03 +0300

    LAB-4 Change assignment number (5 => 4)

--- lab_4/src/task1.cpp
+++ lab_4/src/task1.cpp
@@ -5,12 +5,12 @@ extern "C" uint32_t task1(uint32_t x, uint32_t y);
 
 int main(int argc, char **argv)
 {
-  uint32_t x = 5, y = 10, z;
+  uint32_t x = 1, y = 0, z;
 
   z = task1(x, y);
   std::cout << "ASM: " << z << std::endl;
 
-  z = 657 + 5 * x + 2 * y;
+  z = 4 * x + 234 + y;
   std::cout << "C++: " << z << std::endl;
 
   return 0;
